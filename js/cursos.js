c47afdee3c0e2cc46f21411c5ac115914d47c0da
Henrique Nicacio <nicacio.web@gmail.com>
2016-12-31 16:40:20 -0200

    Add files via upload

---
+++ js/cursos.js
@@ -0,0 +1,41 @@
+var cursos = [
+    {   nomeCurso: "Desenvolvimento Espiritual", 
+        desCargaHoraria: 130, 
+        desRequisitos:"Idade mínima de 15 anos, ter completado os cursos de Novidade de Vida, Consolidação Individual e ser batizado.",
+        desResumo: "Princípios espirituais colaboram diretamente para santificação e fé. Com uma boa base de conhecimento a respeito da Palavra de Deus, uma pessoa tem condições de crescer e frutificar no Espírito Santo." 
+        +"<br/>O Curso de Desenvolvimento Espiritual tem como objetivo principal o entendimento da  vida cristã, que deve seguir nos princípios de caminhar:<br/>"
+        +"A.No Espírito, crendo nas promessas de Deus por meio de Jesus Cristo e revestimento do Espírito Santo;<br/>"
+        +"B.Em cruz, através do tratamento do caráter, renúncias de pecados e conceitos seculares que impendem a renovação da mente em Cristo;<br/>" 
+        +"C.	Em fé, crendo nas obras e sinais do Senhor na vida daqueles que o amam.<br/>", 
+        modulos: ["", ""] }, 
+    {   nomeCurso: "Formação de Liderança", 
+        desCargaHoraria: 0, 
+        desRequisitos:"",
+        desResumo: "", 
+        modulos: ["", ""] }, 
+    {   nomeCurso: "John", 
+        desCargaHoraria: "Doe", 
+        desRequisitos:"",
+        desResumo: "", 
+        modulos: ["", ""] }, 
+    {   nomeCurso: "John", 
+        desCargaHoraria: "Doe", 
+        desRequisitos:"",
+        desResumo: "", 
+        modulos: ["", ""] }, 
+    {   nomeCurso: "John", 
+        desCargaHoraria: "Doe", 
+        desRequisitos:"",
+        desResumo: "", 
+        modulos: ["", ""] }, 
+    {   nomeCurso: "John", 
+        desCargaHoraria: "Doe", 
+        desRequisitos:"",
+        desResumo: "", 
+        modulos: ["", ""] }, 
+    {   nomeCurso: "John", 
+        desCargaHoraria: "Doe", 
+        desRequisitos:"",
+        desResumo: "", 
+        modulos: ["", ""] }
+    ];
